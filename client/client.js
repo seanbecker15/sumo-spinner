fd245ea700f8bd8b53b8105ae95b3cdc1caf7b40
Meegul <micha3lbilstein@gmail.com>
2017-09-30 11:20:33 -0400

    😎

--- client/client.js
+++ client/client.js
@@ -2,7 +2,7 @@ const display = document.getElementById("canvas");
 const context = display.getContext("2d");
 const socket = io();
 
-const gridSize = 40;
+const gridSize = 1000;
 
 let displaySize = display.width = display.height = Math.min(window.innerWidth, window.innerHeight) * 2;
 let blockSize = displaySize / gridSize;
@@ -10,8 +10,9 @@ let blockSize = displaySize / gridSize;
 display.style.width = (displaySize / 2) + "px";
 display.style.height = (displaySize / 2) + "px";
 
-
+let spinners = {};
 socket.on("update", function (data) {
+    spinners = data.spinners;
     requestAnimationFrame(frame);
 });
 
@@ -23,18 +24,18 @@ socket.emit("joinGame", player);
 
 document.onkeydown = function (event) {
     event = event || window.event;
-    switch(event.keycode) {
+    switch(event.keyCode) {
     case 87:
-        socket.emit('keypress','w');
+        socket.emit('keyPress','w');
         break;
     case 83:
-        socket.emit('keypress','s');
+        socket.emit('keyPress','s');
         break;
     case 65:
-        socket.emit('keypress','a');
+        socket.emit('keyPress','a');
         break;
     case 68:
-        socket.emit('keypress','d');
+        socket.emit('keyPress','d');
         break;
     default:
         break;
@@ -48,7 +49,16 @@ window.onresize = function () {
     display.style.height = (displaySize / 2) + "px";
 };
 
+function drawSpinner(spinner) {
+    //todo change this to drawImage of the actual spinner
+    context.fillStyle = 'rgb(255,0,0)';
+    context.fillRect(spinner.x, spinner.y, 5, 5);
+}
+
 function frame() {
     context.clearRect(0, 0, displaySize, displaySize);
+    for (var key in spinners) {
+		drawSpinner(spinners[key]);
+	}
     context.fill();
 }
